fd5110a2ab7c2a25b4fcd302ac8c8c2a635fef5c
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-03-11 11:32:00 +0900

    able to evaluate bangs

--- monkey/evaluator/evaluator_test.go
+++ monkey/evaluator/evaluator_test.go
@@ -70,3 +70,22 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	}
 	return true
 }
+
+// TestBangOperator ...
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}

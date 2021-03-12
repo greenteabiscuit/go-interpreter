5bdf1d270e52cbf42bf22cc472dd6f3ed735d59e
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-03-12 12:17:04 +0900

    able to evaluate bang operators

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -33,10 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 		evaluated := evaluator.Eval(program)
 		if evaluated != nil {
-			io.WriteString(out, program.String())
+			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
 		}
 	}

19715e53888d56e84940f747919c35d47a26c80e
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2021-03-16 21:18:01 +0900

    if and return statements complete

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -9,6 +9,7 @@ const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ    = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 // Object ...
@@ -58,3 +59,10 @@ func (n *Null) Type() ObjectType {
 func (n *Null) Inspect() string {
 	return "null"
 }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType {return RETURN_VALUE_OBJ}
+func (rv *ReturnValue) Inspect() string {return rv.Value.Inspect()}
